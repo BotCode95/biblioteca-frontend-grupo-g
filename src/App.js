3b533c7b82507d0d28ec22bc065b44f270176818
BotCode95 <patribottino95@gmail.com>
2021-02-11 20:51:21 -0300

    config axios y ruteo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
-
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Layout from './components/layout/Layout'
 function App() {
+
+  console.log(process.env.REACT_APP_BACKEND_URL);
+  
   return (
     <div>
-      <h1>Biblioteca</h1>
+      <Router>
+        <Switch>
+          <Route exact path="/" component= {Layout} />
+        </Switch>
+      </Router>
     </div>
   );
 }
