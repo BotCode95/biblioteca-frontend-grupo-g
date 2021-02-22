6622cbb583ab9bb7cc2c8a767e547dd736ff438e
BotCode95 <patribottino95@gmail.com>
2021-02-22 18:53:40 -0300

    container nuevo libro

--- src/components/libros/NuevoLibro.js
+++ src/components/libros/NuevoLibro.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import Layout from '../layout/Layout'
 
 const NuevoLibro = () => {
     
     return ( 
-        <p>nuevo libro</p>
+        <>
+        <Layout/>
+        <div className="container">
+            <div className="contenedor-form">
+                <p className="">nuevo libro</p>
+            </div>
+        </div>
+        </>
      );
 }
  
